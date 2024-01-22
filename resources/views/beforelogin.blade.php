e27f88079ddec906bb580865e0429c74b4d7ffb2
Nadinefiona <nadinefiona9@gmail.com>
2024-01-22 11:32:38 +0200

    adding some functionalities

---
+++ resources/views/beforelogin.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body >
+    <nav class="nav-links">
+       <ul>
+        <header>FMS</header>
+        <li><a href="">Home</a></li>
+        <li><a href="/blogs">Blogs</a></li>
+        <li><a href="">Contact Us</a></li>
+        <li><a href="">Signup</a></li>
+        <li><a href="/login" id="button">Login</a></li>
+       </ul>
+    </nav>
+    <div class="content">
+        <h1>Login as</h1>
+        <div>Employee <a href=""></a><ion-icon name="arrow-redo-outline"></ion-icon> </div>
+        <div>Veterinary <a href=""><ion-icon name="arrow-redo-outline"></ion-icon></a></div>
+        <div>Customer <a href=""><ion-icon name="arrow-redo-outline"></ion-icon></a></div>
+    </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+</body>
+</html>
